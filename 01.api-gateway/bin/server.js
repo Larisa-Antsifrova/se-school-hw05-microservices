cc1b359879e22e5eca6843992fc2e10998647637
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-09-08 20:47:20 +0300

    Added SIGINT handler.

--- 01.api-gateway/bin/server.js
+++ 01.api-gateway/bin/server.js
@@ -5,13 +5,20 @@ const { Ports } = require('../helpers/constants');
 
 const PORT = process.env.PORT || Ports.default;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
 process.on('SIGTERM', () => {
   console.log('SIGTERM signal received: closing HTTP server');
-  app.close(() => {
+  server.close(() => {
+    console.log('HTTP server closed');
+  });
+});
+
+process.on('SIGINT', () => {
+  console.log('SIGINT signal received: closing HTTP server');
+  server.close(() => {
     console.log('HTTP server closed');
   });
 });
