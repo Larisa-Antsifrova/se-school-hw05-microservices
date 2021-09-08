8968ceebf422c8a07ee7f9495c41b5989c67cdfc
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-09-09 00:33:20 +0300

    Grouped user codebase in user microservice.

---
+++ 04.user-microservice/controllers/user-controllers.js
@@ -0,0 +1,19 @@
+class UserControllers {
+  async get(req, res, next) {
+    return res.send('Not implemented.');
+  }
+
+  async create(req, res, next) {
+    return res.send('Not implemented.');
+  }
+
+  async update(req, res, next) {
+    return res.send('Not implemented.');
+  }
+
+  async delete(req, res, next) {
+    return res.send('Not implemented.');
+  }
+}
+
+module.exports = UserControllers;
